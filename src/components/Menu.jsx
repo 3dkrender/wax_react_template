de3425ea3770eb83bdc1114d016de84c04b3f875
dferna10 <dferna10@estudiantes.unileon.es>
2022-07-28 07:49:44 +0200

    Correccion  final de librerías

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { css } from 'glamor';
 import { useSelector } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 import LogoIcon from '../images/3DK_LOGO_ICON_1300.png';
@@ -15,10 +14,6 @@ export const Menu = (props) => {
     const dispatch = useDispatch();
     const locationHistory = useHistory();
     const UserState = useSelector((store) => store.user);
-
-    const disabledStyle = css({
-        opacity: 0.5
-    })
 
     const handleLogin = () => {
         UserService.login(() => {
@@ -52,14 +47,12 @@ export const Menu = (props) => {
             </div>
             <div className="d-flex">
                 <Link to="/" className="btn-item">Main</Link>
-                <Link to="/home" className={`${(UserState.isLogged) ? '' : disabledStyle} btn-item`}>Home</Link>
-                <Link to="/page2" className={`${(UserState.isLogged) ? '' : disabledStyle} btn-item`}>Page2</Link>
+                <Link to="/home" className={`${(UserState.isLogged) ? '' : "disable"} btn-item`}>Home</Link>
+                <Link to="/page2" className={`${(UserState.isLogged) ? '' : "disable"} btn-item`}>Page2</Link>
                 {
-                    !UserState.isLogged &&
+                    !UserState.isLogged ?
                     <button className="btn-item" onClick={handleLogin}><img src={EnterIcon} alt="Loggin" width="24" /> Login</button>
-                }
-                {
-                    UserState.isLogged &&
+                    :
                     <Link to="/" className="btn-item" onClick={onHandleLogout}>Logout <img src={ExitIcon} alt="Exit" width="24" /></Link>
                 }
             </div>
